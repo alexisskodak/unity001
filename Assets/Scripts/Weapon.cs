b5c35d2b7f128d47a76ac94771240176d54dbc7e
alexis <skodak95alexis@gmail.com>
2020-04-20 15:06:37 +0200

    removed movement behaviour in Weapon.Ccs to be added later on future script. TODO: ArmPivot.cs

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -13,15 +13,6 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {   
-        float cSpeed = animator.GetFloat("Speed");
-
-        if(cSpeed < 0.01f && cSpeed > 0f)
-        {
-            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(-1f, 0f, rotZ);
-        }
-
         if(Input.GetButtonDown("Fire1") && bulletCount > 0)
         {   
             bool shootMode = animator.GetBool("ShootMode");
