8b1c8ed2ef93a0b77a088c74bcc15c9fbf132808
alexis <skodak95alexis@gmail.com>
2020-04-20 11:48:49 +0200

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+	public GameObject deathEffect;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	public bool shootMode = false;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+			animator.SetBool("IsCrouching", true);
+		} 
+		
+		else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+			animator.SetBool("IsCrouching", false);
+		}
+		SetFireMode();
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+	public void OnCrouching (bool isCrouching)
+	{
+		animator.SetBool("IsCrouching", isCrouching);
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+
+	public void SetFireMode()
+	{
+		if(Input.GetButtonDown("ShootModeToggle") && shootMode == false)
+		{
+			animator.SetBool("ShootMode", true);
+			shootMode = true;
+		}
+		else if (Input.GetButtonDown("ShootModeToggle") && shootMode == true)
+		{
+			animator.SetBool("ShootMode", false);
+			shootMode = false;
+		}
+	}
+
+
+}
+
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{   
+    public int bulletCount;
+    public Transform FirePoint;
+    public Transform BulletPrefab;
+    public Animator animator;
+    //private float timeBtwShots;
+    //public float startTimeBtwShots;
+
+    void Update()
+    {   
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(-1f, 0f, rotZ);
+
+        if(Input.GetButtonDown("Fire1") && bulletCount > 0)
+        {   
+            bool shootMode = animator.GetBool("ShootMode");
+            if (shootMode == true)
+            {
+                Shoot();
+                animator.SetBool("IsShooting", true);
+            }
+        }
+
+        if(Input.GetButtonUp("Fire1"))
+        {
+            animator.SetBool("IsShooting", false);
+        }
+    }
+
+    void Shoot()
+    {   
+        Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+        bulletCount -= 1;
+    }
+}
