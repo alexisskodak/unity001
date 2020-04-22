3fd175c7dde93c62696b8e534a1dea1b2b2b69d9
alexis <skodak95alexis@gmail.com>
2020-04-23 00:43:54 +0200

    Added TMPro

---
+++ Assets/Scripts/UIAmmoDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIAmmoDisplay : MonoBehaviour
+{
+    public Weapon cWeapon;
+    public TextMesh ammoDisplay;
+
+    string ammo;
+    
+    void Start()
+    {
+        cWeapon = GameObject.Find("FirePoint").GetComponent<Weapon>();
+        
+
+    }
+
+    void Update()
+    {
+        ammoDisplay.text = cWeapon.DisplayBulletCounter();
+    }
+}
