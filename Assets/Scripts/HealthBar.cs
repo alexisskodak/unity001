97d3f4462748f71a69818f7740f2872b7c1f6d32
alexis <skodak95alexis@gmail.com>
2020-04-23 00:16:31 +0200

    added SetColor function

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {   
     private Transform bar;
+    private SpriteRenderer hBar;
+
     private void Start()
     {
         bar = transform.Find("Bar");
+        hBar = GameObject.Find("BarSprite").GetComponent<SpriteRenderer>();
     }
 
     public void SetSize(float sizeNormalized)
     {
         bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+
+    public void SetColor(Color inputColor)
+    {
+        hBar.color = inputColor;
     }
 }
