5b2237d3b4d482544ed16161cc6926ac3adeee1f
alexis <skodak95alexis@gmail.com>
2020-04-28 17:21:44 +0200

    Added setters and getters, enemy AI in progress

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
-    public float health = 100;
+{      
+    Rigidbody2D rb2d;
+
+    [SerializeField]
+    Transform player;
+
+    public Animator animator;
     public GameObject deathEffect;
     public HealthBar healthBar;
+
+    float health;
+    float moveSpeed;
+    float agroRange;
+
+    void Start()
+    {   
+        rb2d = GetComponent<Rigidbody2D>();
+        SetHealth(100f);
+    }
+
+    void Update()
+    {
+        // distance to player
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+    }
     
     public void TakeDamage(float damage)
     {
@@ -18,6 +39,34 @@ public class Enemy : MonoBehaviour
         if(health < 30)
             healthBar.SetColor(Color.red);
     }
+
+    public void SetHealth(float hp)
+    {   
+        if(hp >= 0f)
+            health = hp;
+        else
+            health = 50f;
+    }
+
+    public void SetAgroRange(float aRange)
+    {   
+        if(aRange >= 0f)
+            aRange = agroRange;
+        else
+            aRange = 5f;
+    }
+
+    public void SetMovementSpeed(float mSpeed)
+    {   
+        if(mSpeed > 0f)
+            moveSpeed = mSpeed;
+        else
+            moveSpeed = 10f;
+    }
+
+    public float GetHealth() {return health;}
+    public float GetAgroRange() {return agroRange;}
+    public float GetMovementSpeed() {return moveSpeed;}
 
     void Die()
     {
